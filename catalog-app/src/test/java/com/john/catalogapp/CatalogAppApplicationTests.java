72022242c53c00be75b71ee423ef8ed1d5e032dd
John Santiago <johnathan-santiago@outlook.com>
2024-01-05 22:23:12 -0600

    project scaffolding

---
+++ catalog-app/src/test/java/com/john/catalogapp/CatalogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.catalogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
